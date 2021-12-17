16f23fec92684897be161f7d44de96947805020c
jardondaful <68207907+jardondaful@users.noreply.github.com>
2021-12-17 12:05:32 -0800

    Create shiftLeft.py

---
+++ Chapter 4/4.5/shiftLeft.py
@@ -0,0 +1,4 @@
+bit_string = input("Enter a string of bits: ")
+last_letter = bit_string[1:]
+rest_of_bit_string = bit_string[0]
+print("The bit string '" + last_letter + rest_of_bit_string + "' is the bit string you inputted but each bit of it is shifted left by 1")
