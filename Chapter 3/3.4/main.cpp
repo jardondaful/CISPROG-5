3b4932c0f98d0dddd130e173157b7176e2ab3cb2
jardondaful <68207907+jardondaful@users.noreply.github.com>
2021-12-17 11:35:28 -0800

    Create main.cpp

---
+++ Chapter 3/3.4/main.cpp
@@ -0,0 +1,10 @@
+height = int(input("Enter the height from which the ball is dropped: "))
+bounceIndex = float(input("Enter the bounciness index of the ball: "))
+numberOfBounces = int(input("Enter the number of times the ball is allowed to continue bouncing: "))
+distance = 0
+while numberOfBounces > 0:
+  distance = distance + height
+  height = height * bounceIndex
+  distance = distance + height
+  numberOfBounces-= 1
+print("Total distance traveled is: " + str(distance) + "units.")
